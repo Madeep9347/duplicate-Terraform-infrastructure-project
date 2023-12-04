78e35fc8d553194f87d8278053e8ffc4f955585d
Madeep9347 <111452217+Madeep9347@users.noreply.github.com>
2023-12-04 22:38:12 +0530

    Update main.tf

--- module/security/main.tf
+++ module/security/main.tf
@@ -3,36 +3,27 @@ resource "aws_security_group" "ec2_sg" {
   description = "Security group for web instances"
   vpc_id      = var.vpc_id
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] 
-  }
-  ingress {
-    from_port   = 3306
-    to_port     = 3306
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] 
-  }
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Restrict this to specific IPs if possible
-  }
-  ingress {
-    from_port   = 8000
-    to_port     = 8000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Restrict this to specific IPs if possible
+  dynamic "ingress" {
+    for_each = [22, 3306, 80, 8000]
+
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+
+      cidr_blocks = ingress.value == 80 || ingress.value == 8000 ? ["0.0.0.0/0"] : []  # Adjust as needed
+    }
   }
 
-  egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
+  dynamic "egress" {
+    for_each = [0]
+
+    content {
+      from_port   = egress.value
+      to_port     = egress.value
+      protocol    = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
   }
 }
 
@@ -41,17 +32,25 @@ resource "aws_security_group" "rds_sg" {
   description = "Security group for database instances"
   vpc_id      = var.vpc_id
 
-  ingress {
-    from_port       = 3306
-    to_port         = 3306
-    protocol        = "tcp"
-    security_groups = [aws_security_group.ec2_sg.id]  # Reference the EC2 security group ID that allows traffic to RDS
+  dynamic "ingress" {
+    for_each = [3306]
+
+    content {
+      from_port         = ingress.value
+      to_port           = ingress.value
+      protocol          = "tcp"
+      security_groups   = [aws_security_group.ec2_sg.id]  # Reference the EC2 security group ID that allows traffic to RDS
+    }
   }
 
-  egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
+  dynamic "egress" {
+    for_each = [0]
+
+    content {
+      from_port   = egress.value
+      to_port     = egress.value
+      protocol    = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
   }
 }
