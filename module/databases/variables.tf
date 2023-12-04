c9ef8568bc8059de19c040ec1e15e8322720ab67
Madeep9347 <111452217+Madeep9347@users.noreply.github.com>
2023-12-04 17:16:26 +0530

    Update variables.tf

--- module/databases/variables.tf
+++ module/databases/variables.tf
@@ -15,55 +15,55 @@ variable "rds_sg_id" {
 
 variable "allocated_storage" {
   type    = number
-  default = "" # Replace with your desired default allocated storage value
+  
 }
 
 variable "db_identifier" {
   type    = string
-  default = ""  # Replace with your desired default DB identifier
+  
 }
 
 variable "db_name" {
   type    = string
-  default = ""  # Replace with your desired default DB name
+  
 }
 
 variable "db_engine" {
   type    = string
-  default = ""  # Replace with your desired default DB engine
+  
 }
 
 variable "engine_version" {
   type    = string
-  default = ""  # Replace with your desired default engine version
+  
 }
 
 variable "instance_class" {
   type    = string
-  default = ""  # Replace with your desired default instance class
+  
 }
 
 variable "manage_master_user_password" {
   type    = bool
-  default = ""  # Replace with your desired default value
+  
 }
 variable "db_username" {
   type    = string
-  default = ""  # Replace with your desired default username
+  
 }
 
 variable "parameter_group_name" {
   type    = string
-  default = ""  # Replace with your desired default parameter group name
+  
 }
 variable "skip_final_snapshot" {
   type    = bool
-  default = ""  # Replace with your desired default value
+  
 }
 
 variable "final_snapshot_identifier" {
   type    = string
-  default = ""  # Replace with your desired default final snapshot identifier
+  
 }
 
 # ... rest of the configuration
