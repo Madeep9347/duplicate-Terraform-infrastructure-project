7cda464c1d636a3f0343f0888beaa14faca0ac04
Madeep9347 <111452217+Madeep9347@users.noreply.github.com>
2023-12-04 16:35:28 +0530

    Update main.tf

--- module/databases/main.tf
+++ module/databases/main.tf
@@ -1,5 +1,3 @@
-// databases/main.tf
-
 resource "aws_db_subnet_group" "example" {
   name        = "my-new-db-subnet-group-12"
   description = "my-new-db-subnet-group-12"
@@ -15,18 +13,19 @@ resource "aws_kms_key" "example" {
 }
 
 resource "aws_db_instance" "default" {
-  allocated_storage             = 10
-  identifier                    = "rds-db"
-  db_name                       = "mydb"
-  engine                        = "mysql"
-  engine_version                = "5.7"
-  instance_class                = "db.t3.micro"
-  manage_master_user_password   = true
+  allocated_storage             = var.allocated_storage
+  identifier                    = var.db_identifier
+  db_name                       = var.db_name
+  engine                        = var.db_engine
+  engine_version                = var.engine_version
+  instance_class                = var.instance_class
+  manage_master_user_password   = var.manage_master_user_password
   master_user_secret_kms_key_id = aws_kms_key.example.key_id
-  username                      = "foo"
-  parameter_group_name          = "default.mysql5.7"
-  vpc_security_group_ids        = [var.rds_sg_id]
+  username                      = var.db_username
+  parameter_group_name          = var.parameter_group_name
   db_subnet_group_name          = aws_db_subnet_group.example.name
-  skip_final_snapshot           = true  # Avoid creating a final snapshot during deletion
-  final_snapshot_identifier     = "rds-db-snapshot5"  # Provide a different identifier
+  vpc_security_group_ids        = [var.rds_sg_id]
+  skip_final_snapshot           = var.skip_final_snapshot
+  final_snapshot_identifier     = var.final_snapshot_identifier
 }
+
