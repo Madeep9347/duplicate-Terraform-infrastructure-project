a94fbab65722caea065e95b1d587eec5e7b4b348
Madeep9347 <111452217+Madeep9347@users.noreply.github.com>
2023-12-04 17:32:47 +0530

    Update main.tf

--- module/instances/main.tf
+++ module/instances/main.tf
@@ -2,17 +2,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_dynamodb_table" "terraform_lock" {
-  name         = "terraform-lock"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
-
 resource "aws_instance" "example" {
   count                = var.instance_count
   ami                  = var.ami
