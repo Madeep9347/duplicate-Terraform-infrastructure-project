8c2d9efa1904f22eccc4856a2db9a48c2776d4a2
Madeep9347 <111452217+Madeep9347@users.noreply.github.com>
2023-12-04 16:12:42 +0530

    Update variables.tf

--- module/instances/variables.tf
+++ module/instances/variables.tf
@@ -4,4 +4,23 @@ variable "private_subnet_ids" {}
 variable "ec2_sg_id" {
   description = "Security Group ID for RDS"
 }
+variable "ami" {
+  type    = string
+  default = ""  # Replace with your default AMI ID
+}
+
+variable "instance_type" {
+  type    = string
+  default = ""  # Replace with your default instance type
+}
+
+variable "key_name" {
+  type    = string
+  default = ""  # Replace with your default key name
+}
+
+variable "iam_instance_profile" {
+  type    = string
+  default = ""  # Replace with your default IAM instance profile
+}
 
